4c037b3cca9528a9fae8e0e31b26c1dcb60e5d6e
Rodrigo Rodrigues <117953538+RodrigoORodrigues@users.noreply.github.com>
2026-03-25 12:51:17 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,18 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',  // Raiz do projeto
   server: {
     port: 5173,
     open: true
   },
   build: {
     outDir: 'dist',
-    sourcemap: false
+    sourcemap: false,
+    rollupOptions: {
+      input: {
+        main: 'index.html'  // Especifica o entry point
+      }
+    }
   }
 })
